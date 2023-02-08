dce8643a26d3392a286ad28430b54e15f0ce6f42
wagner <wagncarv@outlook.com>
2023-02-08 11:39:05 -0300

    Inicial

---
+++ src/main/java/com/home/algamoney/AlgamoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.home.algamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/algamoney/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.algamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
