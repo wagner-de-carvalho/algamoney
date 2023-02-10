172d5ea5402f2fbafea253b8bf2aaf0963622d94
wagner <wagncarv@outlook.com>
2023-02-10 09:43:12 -0300

    5.6-regra para não salvar pessoa inativa

--- src/main/java/com/home/algamoney/resource/LancamentoResource.java
+++ src/main/java/com/home/algamoney/resource/LancamentoResource.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
 import com.home.algamoney.api.service.LancamentoService;
 import com.home.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
