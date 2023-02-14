d75d49fd490f8c81d46d9f078f408ee7082183ec
wagner <wagncarv@outlook.com>
2023-02-14 16:10:27 -0300

    7.8-ajustando cep

--- src/main/java/com/home/algamoney/repository/PessoaRepository.java
+++ src/main/java/com/home/algamoney/repository/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.home.algamoney.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.home.algamoney.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
--- src/main/java/com/home/algamoney/resource/PessoaResource.java
+++ src/main/java/com/home/algamoney/resource/PessoaResource.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -72,6 +75,11 @@ public class PessoaResource {
 
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
 
+	}
+	
+	@GetMapping
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable) {
+	    return pessoaRepository.findByNomeContaining(nome, pageable);
 	}
 
 }
