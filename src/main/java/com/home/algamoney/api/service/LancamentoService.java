514a2b35185fb02186ce033294bd3a7017e98be9
wagner <wagncarv@outlook.com>
2023-04-03 18:06:02 -0300

    Atualizar lancamento, lancamento service

--- src/main/java/com/home/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/home/algamoney/api/service/LancamentoService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,6 @@ import com.home.algamoney.model.Lancamento;
 import com.home.algamoney.model.Pessoa;
 import com.home.algamoney.repository.LancamentoRepository;
 import com.home.algamoney.repository.PessoaRepository;
-
 
 @Service
 public class LancamentoService {
@@ -31,6 +31,36 @@ public class LancamentoService {
 		}
 
 		return lancamentoRepository.save(lancamento);
+	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		}
+
+		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		/*
+		 * Optional<Lancamento> lancamentoSalvo = lancamentoRepository.findById(codigo);
+		 * if (lancamentoSalvo.isEmpty()) { throw new IllegalArgumentException(); }
+		 */
+		return lancamentoRepository.findById(codigo).orElseThrow(() -> new IllegalArgumentException());
 	}
 
 }
--- src/main/java/com/home/algamoney/resource/LancamentoResource.java
+++ src/main/java/com/home/algamoney/resource/LancamentoResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -76,6 +77,17 @@ public class LancamentoResource {
 		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	@PutMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
+	public ResponseEntity<Lancamento> atualizar(@PathVariable Long codigo, @Valid @RequestBody Lancamento lancamento) {
+		try {
+			Lancamento lancamentoSalvo = lancamentoService.atualizar(codigo, lancamento);
+			return ResponseEntity.ok(lancamentoSalvo);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 	@DeleteMapping("/{codigo}")
